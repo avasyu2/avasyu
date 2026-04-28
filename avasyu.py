58a2688c05c4a2fc2d2474c28263ad0c0d49cb71
avasyu2 <mail.avasyu@gmail.com>
2026-04-28 08:18:16 +0530

    Create avasyu.py

---
+++ avasyu.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("My first streamlit app")
+st.write("Hello! Creating a simple web application using streamlit library.")
+name = st.text_input("Enter your name :")
+age= st.number_input("Enter your age")
+if st.button ("Submit"):
+   st.write("Hello, {name}! Welcome to streamlit
