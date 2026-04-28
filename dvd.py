56bd4f69b64a58a0e80073656fb2dcb88c92dbf5
avasyu2 <mail.avasyu@gmail.com>
2026-04-28 09:00:47 +0530

    Create dvd.py

---
+++ dvd.py
@@ -0,0 +1,16 @@
+<style>
+  body { margin: 0; background: #000; overflow: hidden; }
+  #dvd {
+    position: absolute;
+    width: 100px;
+    height: 50px;
+    background: #00f; /* Blue color */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    font-weight: bold;
+    font-family: sans-serif;
+  }
+</style>
+<div id="dvd">DVD</div>
