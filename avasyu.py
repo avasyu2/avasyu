35c06a40b82a70507c406d36138c87db825ab116
avasyu2 <mail.avasyu@gmail.com>
2026-04-28 08:24:43 +0530

    Update avasyu.py

--- avasyu.py
+++ avasyu.py
@@ -4,4 +4,4 @@ st.write("Hello! Creating a simple web application using streamlit library.")
 name = st.text_input("Enter your name :")
 age= st.number_input("Enter your age")
 if st.button ("Submit"):
-   st.write("Hello, {name}! Welcome to streamlit
+   st.write("Hello, {name}! Welcome to streamlit.")
